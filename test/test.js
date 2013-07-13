d4bf971b6f694b39e151a3cfd6808dc30515cb47
haoxin <coderhaoxin@outlook.com>
2013-07-13 13:31:49 +0800

    add test script

---
+++ test/test.js
@@ -0,0 +1,134 @@
+var should = require('should');
+
+var userId = '777114815379377151';
+var Push = require('../index');
+
+var pushOption = {
+  apiKey: 'gCRSnFvAvWGu3B9yATYt4vTa',
+  secretKey: 'RVSSPAYLGMtORyoqoeeZbUGEeXk31IkG'
+}
+
+var client = new Push(pushOption);
+
+testTag = {};
+testTag.name = 'test-tag';
+
+describe('test baidu push', function () {
+  it('should push message success', function (done) {
+    var option = {
+      push_type: 1,
+      user_id: userId,
+      messages: JSON.stringify(["hello"]),
+      msg_keys: JSON.stringify(["title"])
+    }
+
+    client.pushMessage(option, function(error, result) {
+      if (error) console.log(error);
+      result.response_params.success_amount.should.equal(1);
+      done();
+    })
+  })
+
+  it('should fetch user tag', function (done) {
+    var option = {
+      user_id: userId
+    }
+    client.fetchTag(option, function(error, result) {
+      if (error) return console.log(error);
+      result.response_params.total_num.should.above(-1);
+      done();
+    })
+  })
+
+  it('should fetch all tag', function (done) {
+    client.fetchTag({}, function(error, result) {
+      if (error) return console.log(error);
+      result.response_params.total_num.should.above(-1);
+      done();
+    })
+  })
+
+  it('should set tag for user', function (done) {
+    var option = {
+      tag: testTag.name,
+      user_id: userId
+    }
+    client.setTag(option, function(error, result) {
+      if (error) return console.log(error);
+      done();
+    })
+  })
+
+  it('should fetch user tag', function (done) {
+    var option = {
+      user_id: userId
+    }
+    client.fetchTag(option, function(error, result) {
+      if (error) return console.log(error);
+      result.response_params.total_num.should.above(0);
+      var tags = result.response_params.tags;
+      tags.forEach(function (tag) {
+        if (tag.name === testTag.name) {
+          testTag.tid = tag.tid;
+        }
+      })
+      testTag.should.have.property('tid');
+      done();
+    })
+  })
+
+  it('should push message by tag', function (done) {
+    var option = {
+      push_type: 2,
+      tag: testTag.name,
+      messages: JSON.stringify(["push by tag"]),
+      msg_keys: JSON.stringify(["title"])
+    }
+    client.pushMessage(option, function(error, result) {
+      if (error) return console.log(error);
+      result.response_params.success_amount.should.equal(1);
+      done();
+    })
+  })
+
+  it('should delete user tag', function (done) {
+    var option = {
+      tag: testTag.name,
+      user_id: userId
+    }
+    client.deleteTag(option, function(error, result) {
+      if (error) return console.log(error);
+      done();
+    })
+  })
+
+  it('should fetch user tag', function (done) {
+    var option = {
+      user_id: userId
+    }
+    var flag = 0;
+    client.fetchTag(option, function(error, result) {
+      if (error) return console.log(error);
+      result.response_params.total_num.should.above(-1);
+      var tags = result.response_params.tags;
+      // console.log(tags)
+      tags.forEach(function (tag) {
+        if (tag.name === testTag.name) {
+          flag++;
+        }
+      })
+      // flag.should.equal(0);
+      done();
+    })
+  })
+
+  it('should query bind list success', function () {
+    var option = {
+      user_id: userId
+    }
+    client.queryBindList(option, function(error, result) {
+      if (error) return console.log(error);
+      console.log(result);
+    })
+  })
+})
